24f622bc8a10c31135b84f56aa94388c7f1117e5
nicollaso3DS <nicollasoliveira707@gmail.com>
2024-09-09 11:35:56 -0300

    criando objetos

---
+++ src/main/java/br/sesi/poo/Main.java
@@ -0,0 +1,11 @@
+package br.sesi.poo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.cabeca.olho.corIris = "marrom";
+
+
+    }
+}
---
+++ src/main/java/br/sesi/poo/Pessoa.java
@@ -0,0 +1,12 @@
+package br.sesi.poo;
+
+import br.sesi.poo.cabeca.Cabeca;
+import br.sesi.poo.inferior.MembrosInferiores;
+import br.sesi.poo.tronco.Tronco;
+
+
+public class Pessoa {
+    public Cabeca cabeca;
+    public Tronco tronco;
+    public MembrosInferiores inferior;
+}
